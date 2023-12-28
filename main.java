58be73f91aaf3586c877b63c0765eff0b39bf8fd
Benny Malberg <bmalberg@protonmail.com>
2023-12-28 15:11:30 -0800

    cleaned up and added readme

--- main.java
+++ main.java
@@ -14,40 +14,6 @@ class Main {
         potionShop.addItem("dimate", 20, 25);
         potionShop.addItem("trimate", 301, 12);
 
-        // tests
-        // System.out.println("Initial Inventory: " + traveller.inventory);
-        // traveller.addItem("monomate");
-        // System.out.println("Inventory: " + traveller.inventory);
-        // traveller.removeItem("Dimate");
-        // System.out.println("Inventory: " + traveller.inventory);
-        // System.out.println(traveller.meseta);
-        // traveller.addMeseta(1234);
-        // System.out.println(traveller.meseta);
-        // traveller.removeMeseta(100);
-        // System.out.println(traveller.meseta);
-        // traveller.printBackpack();
-        // Map<String, Integer> result = traveller.backpackInventory();
-        // System.out.println(result);
-        // System.out.println("POTION SHOP");
-        // System.out.println(potionShop.inventory);
-
-        // System.out.println(potionShop.inventory);
-        // potionShop.sellItem("monomate");
-        // potionShop.sellItem("dimate");
-        // potionShop.sellItem("oogabooga");
-        // System.out.println(potionShop.inventory);
-        // potionShop.buyItem("oogabooga");
-        // potionShop.buyItem("monomate");
-        // System.out.println(potionShop.inventory);
-        // System.out.println(potionShop.priceCheck("dimate"));
-        // System.out.println(potionShop.quantityCheck("dimate"));
-        // System.out.println(potionShop.quantityCheck("monomate"));
-        // System.out.println(potionShop.quantityCheck("dimatefjdklfsjd"));
-        // potionShop.printInventory();
-        // System.out.println(InventoryUtils.checkItemExistence("MoNomAte",
-        // potionShop.inventory));
-        // System.out.println(InventoryUtils.checkItemExistence("",
-        // potionShop.inventory));
         boolean exitShop = false;
         while (!exitShop) {
             String userInput = MenuHandler.mainMenu();
@@ -87,18 +53,7 @@ class Main {
                     System.out.println("Invalid option.");
             }
         }
-
-        // tests
-        // System.out.println("travller inventory: " + traveller.inventory);
-        // System.out.println(InventoryUtils.checkTravellerHasItem("Monomate",
-        // traveller.backpackInventory()));
-        // System.out.println(InventoryUtils.checkTravellerHasItem("poopy",
-        // traveller.backpackInventory()));
-        // System.out.println(InventoryUtils.checkTravellerAffordItem("Trimate", 42,
-        // potionShop.priceCheck("Trimate")));
-        // InventoryUtils.checkUserBackpackLimit(traveller.backpackInventory());
-        // MenuHandler.buyMenu(traveller.meseta, potionShop.inventory);
-        // MenuHandler.sellMenu(traveller.meseta, traveller.inventory);
+        MenuHandler.closeScanner();
 
     }
 
